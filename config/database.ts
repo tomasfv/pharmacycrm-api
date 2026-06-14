15e27906e8d849ae4887da2ce3261654d8104935
tomasfv <tomasfernandezvaldes@gmail.com>
2026-06-14 16:54:58 -0300

    add deploy

--- config/database.ts
+++ config/database.ts
@@ -3,19 +3,26 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME!,
-  process.env.DB_USER!,
-  process.env.DB_PASSWORD,
-  {
-    host: process.env.DB_HOST,
-    port: parseInt(process.env.DB_PORT || '5432', 10),
-    dialect: 'postgres',
-    logging: false,
-    define: {
-      timestamps: true,
-    },
-  }
-);
+const sequelize = process.env.DB_DEPLOY
+  ? new Sequelize(process.env.DB_DEPLOY, {
+      dialect: 'postgres',
+      logging: false,
+      dialectOptions: {
+        ssl: { require: true, rejectUnauthorized: false },
+      },
+      define: { timestamps: true },
+    })
+  : new Sequelize(
+      process.env.DB_NAME!,
+      process.env.DB_USER!,
+      process.env.DB_PASSWORD,
+      {
+        host: process.env.DB_HOST,
+        port: parseInt(process.env.DB_PORT || '5432', 10),
+        dialect: 'postgres',
+        logging: false,
+        define: { timestamps: true },
+      }
+    );
 
 export default sequelize;
